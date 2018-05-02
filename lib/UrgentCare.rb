9777bcdb7b8765e4adcb8888fe9d84c0ab3f4fe6
Pamela Torres-Rocca <pamelafrieze@Pamelas-MacBook-Air.local>
2018-05-02 18:58:03 -0400

    Addt of requirements

--- lib/UrgentCare.rb
+++ lib/UrgentCare.rb
@@ -1,6 +1,9 @@
+require 'pry'
+require 'nokogiri'
+
+require_relative "./UrgentCare/CLI"
 require_relative "./UrgentCare/version"  #why was this not a relative path?
 #required by bundler
 module UrgentCare
   # Your code goes here...
-
 end
