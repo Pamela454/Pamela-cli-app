326b1486f313445fd290003cb42bab10467064f7
Pamela Torres-Rocca <pamelafrieze@Pamelas-MacBook-Air.local>
2018-05-02 21:23:25 -0400

    Office Class Outline

--- lib/UrgentCare/Office.rb
+++ lib/UrgentCare/Office.rb
@@ -1,2 +1,32 @@
-class UrgentCare::Office
+class UrgentCare::Office  #class to collect
+  attr_accessor :name, :url, :address, :phone_number
+
+  def initialize(name)
+    @name = name
+    @url = url
+    @address = address
+    @phone_number = phone_number
+  end
+
+  #def self.all
+
+  #end
+
+  #def doc
+
+  #end
+
+  #def scrape_office_site
+
+  #end
+
+  #def address
+
+  #end
+
+  #def phone_number
+
+  #end
+
+
 end
