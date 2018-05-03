9cbce58076c2fe7901c75d65d82e8bbccf331ddb
Pamela Torres-Rocca <pamelafrieze@Pamelas-MacBook-Air.local>
2018-05-03 13:48:15 -0400

    initial office list scraping

--- lib/Urgentcare/Scraper.rb
+++ lib/Urgentcare/Scraper.rb
@@ -4,8 +4,16 @@ require 'nokogiri'
 require 'pry'
 require 'open-uri'
 
-  def first_page
+  def first_page #clinics in Boston and surrounding area
     @doc = Nokogiri::HTML(open("https://www.carewellurgentcare.com/locations/"))
+  end
+
+  def office_list
+    first_page.css('.et_pb_code_inner').css("strong").text.gsub()
+  end
+
+  def individual_office
+
   end
 
 #end
