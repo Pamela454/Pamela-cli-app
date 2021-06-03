1fb582d86677353df35f32f968d0eb7e8f06d392
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-06-03 11:41:32 -0400

    phone number gsub correction

--- lib/Urgentcare/Scraper.rb
+++ lib/Urgentcare/Scraper.rb
@@ -31,7 +31,7 @@ class Urgentcare::Scraper
     office = Urgentcare::Office.new
     office.name = office_details.css('h2').text
     office.url = office_details.css('a')[2]['href']
-    office.phone_number = office_details.css('a[href]').text.delete "Book Telemed AppointmentSchedule AppointmentGet Directions"
+    office.phone_number = office_details.css('a[href]').text.gsub("Get DirectionsBook Urgent Care AppointmentBook COVID-19 TestBook Telemed Appointment", " ")
     office.next_available = get_waittime.css('strong')[index].text
   end
 end
