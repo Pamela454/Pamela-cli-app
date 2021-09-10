728c6798698368c5a26c79dedf5fae3ed2f0398c
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2021-09-09 22:17:25 -0400

    Office class specs

--- spec/CLI_spec.rb
+++ spec/CLI_spec.rb
@@ -1,17 +1,32 @@
 require './lib/UrgentCare/CLI.rb'
+require 'stringio'
+
+#use test doubles so internet conn not necessary 
+#double - simple object with preprogrammed expectations and responses as preparation for calls it will receive
+#stub - an instruction to an object to return a specific response to a method call 
 
 RSpec.describe Urgentcare::CLI do 
 
+	let!(:output) { StringIO.new }
+	let!(:cliInstance) {Urgentcare::CLI.new}
+
     it "calls the welcome method" do
-     cliInstance = Urgentcare::CLI.new
      expect(cliInstance).to receive(:welcome).once
      cliInstance.call
     end
 
+    it "prints a welcome message" do 
+     expect(cliInstance.welcome).to output(
+     	a_string_including("Welcome to the Urgent Care CLI")
+     	).to_stdout 
+    end
+
+    #want to receive message that is a response to user entry of exit 
     it 'displays message if user exits' do 
-      cliInstance = Urgentcare::CLI.new
       allow(:cliInstance).to receive(:list)
-      allow($stdout).to receive(:puts) #receive input from user 
-      expect(:list("exit")).to eq("Thank you and Goodbye!")
+      #expect(:list("exit")).to eq("Thank you and Goodbye!")
     end
 end
+
+#check that it retrives list 
+#check that it responds to user input
