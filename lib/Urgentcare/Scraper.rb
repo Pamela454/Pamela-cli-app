423e0e1a79276f49c5bb6defa429179023d92dc6
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-02-24 10:15:05 -0500

    get_apptime rename and refactor

--- lib/Urgentcare/Scraper.rb
+++ lib/Urgentcare/Scraper.rb
@@ -49,7 +49,7 @@ class Urgentcare::Scraper
   def get_clinic_site 
     @@browser.goto(@@url[$index]) 
     @js_doc = @@browser.iframe.wait_until(&:present?) 
-    get_appttime
+    get_appttime_date
   end
 
   def make_office(office_details)
@@ -62,16 +62,17 @@ class Urgentcare::Scraper
     @off.phone_number = office_details.css('a[href]').text.gsub("Get DirectionsBook Urgent Care Appointment", " ")
   end
 
-  def get_appttime #retrieve waittime and add to new office model 
+  def get_appttime_date #retrieve waittime and add to new office model 
     sleep(1);
    iframe_info = Nokogiri::HTML(@js_doc.html) 
     if iframe_info.css('div.time-slots')
       @wait_day = iframe_info.css('div.d-flex.d-md-none.header').text
       @wait_time = iframe_info.css('div.time-slot.selected').text
+      @office.all[$index].next_available = "#{@wait_day} #{@wait_time}"
     else   
       @wait_time = "No time available"
+      @office.all[$index].next_available = "#{@wait_time}"
     end
-      @office.all[$index].next_available = "#{@wait_day} #{@wait_time}"
   end
 
 end  
