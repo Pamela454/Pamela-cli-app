8bef00cc703c634b4b22a5bdd75820980b0dfbcc
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2023-03-09 10:43:11 -0500

    v0.2.5

--- lib/UrgentCare/version.rb
+++ lib/UrgentCare/version.rb
@@ -1,3 +1,3 @@
 module Urgentcare
-  VERSION = "0.2.4"  #increment a version with each release
+  VERSION = "0.2.5"  #increment a version with each release
 end                  #semantic versioning 
